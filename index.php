fe5e99423cb3026e297365aa07dba52be134f5c0
Niklas <nachtmahr2010@googlemail.com>
2015-06-08 16:35:03 +0200

    index.php
    Very first version of the index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>OOP-Ausgabe</title>
+	</head>
+	<body>
+		<div id="top" style="background-color: #FF0000;">
+			<h1>Headline</h1>
+		</div>
+
+		<div id="main" style="background-color: #00FF00;">
+			<p>
+				Haupttext
+			</p>
+		</div>
+
+		<div id="bottom" style="background-color: #0000FF;"
+			<p>30.06.2015 Hausarbeit im Modul Objektorientierte Programmierung von: 
+				Bendixen, Niklas; Horn, Moritz; Peters, Henning Renke</p>
+		</div>
+	</body>
+</html>
